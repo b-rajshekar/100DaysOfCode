e04e715a2eef90d1da16668f9ca2f33663f7511d
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-23 11:09:30 +0530

    Day1done

---
+++ day1/BandNameGenerator(project1).py
@@ -0,0 +1,8 @@
+print("Welcome to the IPL Team Name Generator.")
+
+cityName = input("What's the name of the city you grew up in or you can type your favourite city name?\n")
+endingName = input("Choose a dashing name for your team?\n")
+
+
+
+print("Your Team name could be " + cityName + " " + endingName)
---
+++ day1/functions&variables.py
@@ -0,0 +1,19 @@
+# print function
+# print("Hello World")
+
+# input function
+# input("Hola "+ input("What is your name: "))
+
+# variables
+# name = input("What is your name? ")
+# print(name)
+
+name = "Jack"
+print(name)
+
+name = "Raj"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
