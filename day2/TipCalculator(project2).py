ee7ede42d51517fcfe753d89d5adb93c76e22d5c
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-24 11:35:37 +0530

    changed input

--- day2/TipCalculator(project2).py
+++ day2/TipCalculator(project2).py
@@ -1,7 +1,7 @@
 print("Welcome to the tip calculator!")
 
 bill = float(input("What was the total bill? ₹"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+tip = int(input("How much tip would you like to give? 10, 12, 15 or nothing? "))
 people = int(input("How many people to split the bill? "))
 
 tip_as_percent = tip / 100
