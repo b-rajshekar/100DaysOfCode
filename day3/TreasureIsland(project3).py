5efd029b754050d7b3e5a90895c64a0a79603e4a
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-26 09:48:38 +0530

    error fixed

--- day3/TreasureIsland(project3).py
+++ day3/TreasureIsland(project3).py
@@ -32,8 +32,9 @@ choice1 = input('"You\'re at a crossroad, where do youwant to go? Type "left" or
 if choice1 == "left":
     choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
     if choice2 == "wait":
-       choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
+       choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
        if choice3 == "red":
+         print("It's a room full of fire. Game Over.")
        elif choice3 == "yellow":
          print("You found the treasure! You Win!")
        elif choice3 == "blue":
