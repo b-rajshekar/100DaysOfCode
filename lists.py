24a3fd1f97a23715e7cd75cb828d958a02189b72
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-26 10:57:29 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,35 @@
+states_of_india = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    " Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+]
+
+# Print the number of states in the list
+number_of_states = len(states_of_india)
+print(number_of_states)
