87ec7dc99d784aca40f5fc6e4badb3f534941d21
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-27 08:52:57 +0530

    loopswithLists

---
+++ day5/loopsLists.py
@@ -0,0 +1,10 @@
+# For Loop
+
+'''for item in list_of_items:
+    Do Something to each item'''
+    
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
