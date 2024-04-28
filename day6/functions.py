b105c8579c4d1826717c999896dab29cd2bc58ff
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-28 18:20:16 +0530

    function

---
+++ day6/functions.py
@@ -0,0 +1,18 @@
+# Defining Functions
+
+# def my_function():
+   # Do this
+   # Then do this
+   # Finally do this
+   
+# calling Functions
+# my_function()
+
+
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+    
+my_function()
