a1c65db2e4965940e7e85c8987f0abd6edc9eed7
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-28 23:32:24 +0530

    whileloop

---
+++ day6/whileLoop.py
@@ -0,0 +1,11 @@
+# For
+# for item in list_of_items:
+  #Do something to each item
+  
+# for number in range(a,b):
+#   print(number)
+
+
+# While
+# while something_is_true:
+  #Do something repeatedly
