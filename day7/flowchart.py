75d2e77df6e226e313502b0493691078d4d8739a
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-29 11:13:33 +0530

    flowChart

---
+++ day7/flowchart.py
@@ -0,0 +1,24 @@
+'''
+START
+
+Generate a random word
+
+Generate as many blanks as letters in word
+
+Ask the user to guess a letter
+
+
+            is the guessed letter in the word?
+    Yes                                             No
+    
+    Replace the blank with the letter                  Lose a life
+    
+    
+    Are all the blanks filled?                          Have they run out of lives
+    No                                              No
+    
+    Yes                                             Yes
+    
+                            Game Over
+                            
+    '''
