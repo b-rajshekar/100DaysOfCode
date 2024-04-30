d325da357c9722a904d479328e560037d0cbe63e
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-30 23:49:06 +0530

    keepTrackofthe player live

--- day7/pickRandomWord.py
+++ day7/pickRandomWord.py
@@ -1,30 +1,101 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+
+lives = 6
+
+#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 for _ in range(word_length):
     display += "_"
 
-
-end_of_game = False
-
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    
+
     for position in range(word_length):
         letter = chosen_word[position]
-        
+
         if letter == guess:
             display[position] = letter
 
-    print(display)
 
-    
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
+
+    print(stages[lives])
