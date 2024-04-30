ea32c534288369914769d5ce52e7424b0579fc9f
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-04-30 23:42:01 +0530

    check player win

--- day7/pickRandomWord.py
+++ day7/pickRandomWord.py
@@ -1,19 +1,30 @@
 import random
-word_list = ["ardvark", "baboon", "camel"]
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-print(f"Pssst, the solution is {chosen_word}.")
+print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
-print(display)
 
-guess = input("Guess a letter: ").lower()
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] =  letter
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
         
-print(display)
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
