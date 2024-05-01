cf76d1eb2d6e2d423f7a1d4e026fe0f6c921a80d
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-05-01 21:10:16 +0530

    functionsWith Inputs

---
+++ day8/functionsWithInputs.py
@@ -0,0 +1,13 @@
+# Simple Function 
+# def greet():
+#     print("Hola")
+#     print("Como Estas")
+#     print("Hasta la proxima")
+# greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hola {name}")
+    print(f"Como Estas {name}?")
+greet_with_name("raj")
