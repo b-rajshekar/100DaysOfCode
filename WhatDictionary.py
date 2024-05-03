565fcc7e8a0b6dc71b5401ebac4b92738eb6d655
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-05-03 23:08:18 +0530

    dictionary

---
+++ WhatDictionary.py
@@ -0,0 +1,16 @@
+# Dictionary in Python
+
+''' KEY         |       VALUE
+    Bug         |       An error in a program th
+                        at prevents the program from '''
+                        
+                        
+# {Key: Value}
+# Example
+
+{
+"Bug": "An error in a program that prevents the program from running as expected.",
+"Function": "A piece of code that you can easily call over and over again.",
+"Loop": "The action of doing something over and over again.",
+}
+                    
