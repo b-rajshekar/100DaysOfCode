27107d7ceab3b0e3e4258d0f1ebe71e7adf15cc0
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-05-04 21:43:13 +0530

    more on dictionary

--- DictionaryInAction.py
+++ DictionaryInAction.py
@@ -9,4 +9,20 @@ print(coding_dictionary["Bug"])
 
 #Adding new items to dictionary.
 coding_dictionary["spanishWords"] = "Hola, Como estas, Mucho Gusto, Adios."
+
+
+#Create an empty dictionary.
+empty_dictionary = {}
+
+#Wipe an existing dictionary
+# coding_dictionary = {}
+# print(coding_dictionary)
+
+#Edit an item in a dictionary
+coding_dictionary["Bug"] = "A moth in your computer."
 print(coding_dictionary)
+
+#Loop through a dictionary
+for key in coding_dictionary:
+    print(key)
+    print(coding_dictionary[key])
