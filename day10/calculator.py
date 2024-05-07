f3c59782ceded5073bfd66882d3f42ff2598ebe3
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-05-07 12:29:11 +0530

    basic functions

--- day10/calculator.py
+++ day10/calculator.py
@@ -4,3 +4,15 @@
 def add(n1, n2):
     return n1 + n2
 
+#Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
