3ad093f4e980f82ee8c1236846d9191f3dd88349
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-07-04 13:00:40 +0530

    Added Comments to the code so that it will be easy to understand in the future

--- day1/TeamNameGenerator(project1).py
+++ day1/TeamNameGenerator(project1).py
@@ -1,8 +1,12 @@
+# Greets the user
 print("Welcome to the IPL Team Name Generator.")
 
+# Asks the user to provide input
 cityName = input("What's the name of the city you grew up in or you can type your favourite city name?\n")
+
+# Asks the user to provide input
 endingName = input("Choose a dashing name for your team?\n")
 
 
-
+# prints the final output
 print("Your Team name could be " + cityName + " " + endingName)
