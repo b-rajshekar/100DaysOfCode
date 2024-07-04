aebb0c49e21d58a9d2ed09bf702bb44c05d07d98
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-07-04 20:05:08 +0530

    typeError

--- day2/typeErrorCheckingConversion.py
+++ day2/typeErrorCheckingConversion.py
@@ -1,11 +1,11 @@
-# num_char = len(input("What is your name? "))
-# new_num_char = str(num_char)
-# print("Your name has " + new_num_char + " Characters.")
+num_char = len(input("What is your name? "))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " Characters.")
 
 # Type check and conversion
-a = str(123)
-print(type(a))
+# a = str(123)
+# print(type(a))
 
-print( 70 + float("100.5"))
+# print( 70 + float("100.5"))
 
-print(str(30)+str(40))
+# print(str(30)+str(40))
