2ee36701c175e4cd2a3c67d6101e3fe5ec6c964a
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2024-07-07 12:06:54 +0530

    Added comments for better understanding

--- day4/RockPaperScissors.py
+++ day4/RockPaperScissors.py
@@ -34,10 +34,16 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 
+# Takes input from the user and store it in user_choice
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for scissors.\n"))
+
+# Prints user choice
 print(game_images[user_choice])
 
+# It uses the random
 computer = random.randint(0,2)
+
+# Prints computer choice
 print("computer chose: ")
 print(game_images[computer])
 
